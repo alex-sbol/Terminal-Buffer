efe73668ea2fedbb9376aee533e9c711f68486c4
AlexSBol <s.b.sasha155@gmail.com>
2026-03-15 01:00:31 +0100

    fix Cell

--- src/main/kotlin/Cell.kt
+++ src/main/kotlin/Cell.kt
@@ -1,10 +1,11 @@
 package org.example
 
-class Cell() {
-    val char: Char? = null
-    val foreground_color: Colors = Colors.DEFAULT_FOREGROUND
-    val background_color: Colors = Colors.DEFAULT_BACKGROUND
-    val bold: Boolean = false
-    val italic: Boolean = false
-    val underline : Boolean = false
+class Cell(
+    val char: Char? = null,
+    val foreground_color: Colors = Colors.DEFAULT_FOREGROUND,
+    val background_color: Colors = Colors.DEFAULT_BACKGROUND,
+    val bold: Boolean = false,
+    val italic: Boolean = false,
+    val underline : Boolean = false) {
+
 }
